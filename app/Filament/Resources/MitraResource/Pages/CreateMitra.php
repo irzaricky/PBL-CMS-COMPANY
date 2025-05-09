bc1b4d93d3e55c00438228012d432e6d712e2f36
Arsyad Irfan Fauza <68624326+irzaricky@users.noreply.github.com>
2025-05-10 04:17:38 +0700

    Menambahkan redirect ketika create form

--- app/Filament/Resources/MitraResource/Pages/CreateMitra.php
+++ app/Filament/Resources/MitraResource/Pages/CreateMitra.php
@@ -3,10 +3,16 @@
 namespace App\Filament\Resources\MitraResource\Pages;
 
 use App\Filament\Resources\MitraResource;
+use App\Services\FileHandlers\SingleFileHandler;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateMitra extends CreateRecord
 {
     protected static string $resource = MitraResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
