728e144cb718e76e88e52537ec60898a5ebb73db
Arsyad Irfan Fauza <68624326+irzaricky@users.noreply.github.com>
2025-05-09 15:17:30 +0700

    Menambahkan fitur sejarah, visi, dan misi ke profil perusahaan

--- database/seeders/ProfilPerusahaanSeeder.php
+++ database/seeders/ProfilPerusahaanSeeder.php
@@ -12,11 +12,13 @@ class ProfilPerusahaanSeeder extends Seeder
         DB::table('profil_perusahaan')->insert([
             [
                 'id_profil_perusahaan' => 1,
-                'id_galeri' => 1,
                 'nama_perusahaan' => 'Biiscorp',
                 'deskripsi_perusahaan' => 'PT Biiscorp adalah perusahaan teknologi yang bergerak di bidang pengembangan software, konsultasi IT, dan penyediaan solusi teknologi untuk berbagai sektor industri.',
                 'alamat_perusahaan' => 'Jl. Teknologi No. 123, Jakarta Selatan',
                 'email_perusahaan' => 'Biiscorp@gmail.com',
+                'sejarah_perusahaan' => 'Didirikan pada tahun 2020, PT Biiscorp telah berkembang pesat dalam menyediakan solusi teknologi yang inovatif dan berkualitas tinggi.',
+                'visi_perusahaan' => 'Menjadi perusahaan teknologi terkemuka yang memberikan solusi inovatif untuk meningkatkan efisiensi dan produktivitas bisnis.',
+                'misi_perusahaan' => 'Menyediakan solusi teknologi yang berkualitas tinggi, memberikan layanan terbaik kepada pelanggan, dan berkontribusi pada perkembangan industri teknologi di Indonesia.',
             ],
         ]);
     }
