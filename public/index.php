85a1572d4ca4c363fd6ad6073573bcf11183da68
Arsyad Irfan Fauza <68624326+irzaricky@users.noreply.github.com>
2025-05-12 19:13:44 +0700

    Revert "TEST INSTALLER"
    This reverts commit aa8691504852d46fc0b4e42f001018e5f44b50f0.

--- public/index.php
+++ public/index.php
@@ -4,20 +4,14 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Check for missing dependencies before attempting to load Laravel
-if (!file_exists(__DIR__ . '/../vendor/autoload.php')) {
-    require __DIR__ . '/pre-install.php';
-    // The pre-install.php script will exit, so execution won't continue
-}
-
 // Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__ . '/../storage/framework/maintenance.php')) {
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
 // Register the Composer autoloader...
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
-(require_once __DIR__ . '/../bootstrap/app.php')
+(require_once __DIR__.'/../bootstrap/app.php')
     ->handleRequest(Request::capture());
