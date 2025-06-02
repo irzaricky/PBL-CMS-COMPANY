9090a7bc98644a2afda03ff7edf6d8db2f744b5a
nab-cat <diaframptonsees@gmail.com>
2025-06-03 02:34:47 +0700

    small fix

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -236,6 +236,11 @@ class UserResource extends Resource
         ];
     }
 
+     public static function canCreate(): bool
+    {
+        return false;
+    }
+
     public static function getPages(): array
     {
         return [
